65363dcd63a5b7b4bf48a73b98ac132b74716fcf
betato <battlestargatticus@gmail.com>
2020-03-02 14:07:40 -0500

    Add rpm measurement

--- speedometer.ino
+++ speedometer.ino
@@ -1,12 +1,50 @@
-unsigned long lastTickTime = 0;
-int tickCount = 0;
-float velocities[100];
+#define MEASURE_TIME 1000 // Time over which ticks are logged in ms, ticks older than this number will be discarded
+#define MEASURE_TIMES_LENGTH 80
+
+int tickCount = 0; // Number of ticks in a time interval no longer than MEASURE_TIME
+unsigned long measureTimes[MEASURE_TIMES_LENGTH]; // Stores times at which ticks occurred
+int head = 0;
+int tail = 0;
+
+void enqueueTime(float tickTime) {
+  measureTimes[head] = tickTime;
+  head = (head + 1) % MEASURE_TIMES_LENGTH;
+}
+
+void dequeueTime() {
+  tail = (tail - 1) % MEASURE_TIMES_LENGTH;
+}
 
 void speedometerTick() {
-  if (tickCount < 100) {
-    // Tick count should always be less that 100 
-    unsigned long thisTickTime = millis();
-    float velocity = (1000 * METERS_PER_TICK)/(thisTickTime - lastTickTime);
-    lastTickTime = thisTickTime;
+  if (++tickCount < MEASURE_TIMES_LENGTH) {
+    // Tick count should always be less that MEASURE_TIMES_LENGTH, otherwise the array should be made longer 
+    unsigned long timeNow = millis();
+    enqueueTime(timeNow); // Add a tick
+    // Remove ticks older than MEASURE_TIME
+    while (measureTimes[tail] > timeNow + MEASURE_TIME) {
+      dequeueTime();
+      tickCount--;
+    }
+    // Remove ticks older than MEASURE_TIME
+    while (tickCount > 0 && measureTimes[tail] > timeNow + MEASURE_TIME) {
+      dequeueTime();
+      tickCount--;
+    }
+  } else {
+    Serial.println("Tick time array is full!");
+    stopForError();
   }
+}
+
+float getTicksPerSecond() {
+  if (head >= tail) {
+    // Number of ticks in the specified capture time
+    return (1000.0 * tickCount)/(measureTimes[head] - measureTimes[tail]);
+  } else {
+    return 0;
+  }
+}
+
+void getVelocity() {
+  
 }
